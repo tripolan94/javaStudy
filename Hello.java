d834a325aa4a19a96f3668377c20719ecc7365c0
tripolan94 <qkrwlsrb35@naver.com>
2020-08-10 11:16:57 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+ public static void main(String[] args) {
+  System.out.println("Hello, welcome to the java world!");
+  System.out.println("Nice meet you!");
+  int myAge = 30;
+  System.out.println("My Age is " + myAge);
+ }
+}
