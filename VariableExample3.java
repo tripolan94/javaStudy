f00c46e9d9bf765781cf4d8d295474894e75b80f
tripolan94 <qkrwlsrb35@naver.com>
2020-08-11 10:50:10 +0900

    ignore add

---
+++ VariableExample3.java
@@ -0,0 +1,20 @@
+public class VariableExample3 {
+	public static void main(String[] args) {
+		byte b1 = -128;
+		byte b2 = 127;
+		
+		//System.out.println(b2++); // b2 = b2+1;
+		//System.out.println(b2++); // b2 = b2+1;
+		//System.out.println(b2++); // b2 = b2+1;
+		
+		//System.out.println(--b1); // b2 = b2-1;
+		//System.out.println(--b1); // b2 = b2-1;
+		//System.out.println(--b1); // b2 = b2-1;
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(b2++);
+		}
+	}
+}
+
+
